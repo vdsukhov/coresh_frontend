8c9cf4784be9f0075638144824731e6ab4f41d8b
Vladimir Sukhov <vdsukhov@ya.ru>
2024-08-08 21:58:42 -0500

    Added favicon and logo

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import logo from "../assets/coresh.png";
 
 const HeaderComponent = () => {
     return (
-         <nav className="navbar bg-body-tertiary">
+        <nav className="navbar bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                    <h5>CORESH - coregulation search engine</h5>
+                <a className="navbar-brand d-flex align-items-center" href="#">
+                    <img src={logo} alt="Logo" className="me-2" style={{ height: '1.2em' }} />
+                    <h5 className="mb-0">coregulation search engine</h5>
                 </a>
             </div>
-         </nav>
+        </nav >
     );
 }
 
